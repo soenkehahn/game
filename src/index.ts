c41b3ac7c9b18484edf71ff47f17bc717e854e59
Hao Lian <hi@haolian.org>
2022-07-16 18:28:53 -0400

    src/index.ts: scenery, which scales and taunts the user

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
-import { Actor, CollisionType, Color, Engine, Input, Vector } from "excalibur";
+import { Actor, ActorArgs, CollisionType, Color, Engine, Input, Timer, Vector } from "excalibur";
 import { Keys } from "excalibur/build/dist/Input/Keyboard";
 
 const game = new Engine({
   width: 800,
   height: 550,
 });
+
+class Scenery extends Actor {
+  sceneryScale: number = 1;
+  constructor() {
+    super({ x: game.drawWidth / 2, y: game.drawHeight / 2 })
+  }
+
+  public update(_engine: Engine, delta: number): void {
+    this.sceneryScale += delta * 0.0001 * this.sceneryScale;
+    this.scale = new Vector(this.sceneryScale, this.sceneryScale);
+  }
+}
+
+class Wall extends Actor {
+  constructor(x: number) {
+    super({
+      x,
+      y: 0,
+      width: 20,
+      height: 200,
+      color: Color.fromRGB(255, 120, 180),
+    });
+    this.body.collisionType = CollisionType.Fixed;
+  }
+}
 
 class SpaceShip extends Actor {
   constructor() {
@@ -38,9 +63,12 @@ class SpaceShip extends Actor {
 }
 
 const spaceship = new SpaceShip();
-
-game.add(spaceship);
-
+const scenery = new Scenery();
+for (const x of [new Wall(-100), new Wall(100)]) {
+  scenery.addChild(x);
+}
+for (const x of [scenery, spaceship]) {
+  game.add(x);
+}
 game.backgroundColor = Color.Black;
-
 game.start();
