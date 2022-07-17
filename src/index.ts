494038800ed4486cf1eab9f97eaffca60cf52b06
Sönke Hahn <soenkehahn@gmail.com>
2022-07-16 21:24:26 -0400

    Zoom out to get rid of some of the jankiness

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,14 @@ import {
 } from "excalibur";
 import { Keys } from "excalibur/build/dist/Input/Keyboard";
 
-const CONFIG = {
-  shipVelocity: 0.4,
-  shipSize: 20,
-};
-
 const TAU = Math.PI * 2;
+
+class CONFIG {
+  static zoom = 0.01;
+  static shipVelocity = 0.4 / CONFIG.zoom;
+  static shipSize = 20 / CONFIG.zoom;
+  static spiralAngle = -TAU * 0.24;
+}
 
 const game = new Engine({
   width: 800,
@@ -33,7 +35,7 @@ class Scenery extends Actor {
 
   public update(_engine: Engine, delta: number): void {
     this.sceneryScale += delta * 0.0002 * this.sceneryScale;
-    game.rootScene.camera.zoom = this.sceneryScale;
+    game.rootScene.camera.zoom = this.sceneryScale * CONFIG.zoom;
     spaceship.scale = vec(1 / this.sceneryScale, 1 / this.sceneryScale);
     spaceship.velocity = CONFIG.shipVelocity / this.sceneryScale;
   }
@@ -44,15 +46,17 @@ class Wall extends Actor {
     start,
     end,
     thiccness,
+    nextThiccness,
   }: {
     start: Vector;
     end: Vector;
     thiccness: number;
+    nextThiccness: number;
   }) {
     super({
       x: (start.x + end.x) / 2,
       y: (start.y + end.y) / 2,
-      width: start.distance(end),
+      width: start.distance(end) + Math.max(1, nextThiccness),
       height: Math.max(1, thiccness),
       color: Color.fromRGB(255, 120, 180),
       rotation: end.sub(start).toAngle(),
@@ -101,17 +105,19 @@ const spaceship = new SpaceShip();
 const scenery = new Scenery(spaceship);
 
 let position = vec(0, 0);
-let direction = vec(100, 0).rotate(TAU / 8);
-for (let i = 0; i < 21; i++) {
+let direction = vec(100 / CONFIG.zoom, 0).rotate(TAU / 8);
+for (let i = 0; i < 100; i++) {
   const end = position.add(direction);
+  const nextDirection = direction.rotate(CONFIG.spiralAngle).scale(0.8);
   const wall = new Wall({
     start: position,
     end,
     thiccness: direction.distance(Vector.Zero) / 10,
+    nextThiccness: nextDirection.distance(Vector.Zero) / 10,
   });
   scenery.addChild(wall);
   position = end;
-  direction = direction.rotate(-TAU * 0.24).scale(0.8);
+  direction = nextDirection;
 }
 
 for (const x of [scenery, spaceship]) {
